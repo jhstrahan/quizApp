18c62f7bf20d2619b020b8dce2e74e9f16b14f82
Joel Strahan <joeltstrahan@gmail.com>
2017-06-23 11:27:15 -0500

    Initial Commit

---
+++ TrueFalseStarter/QuestionData.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionData.swift
+//  TrueFalseStarter
+//
+//  Created by Joel Strahan on 6/23/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+import GameKit
+
+struct QuestionData {
+    
+    let questionData = [""]
+}
---
+++ TrueFalseStarter/Questions.swift
@@ -0,0 +1,19 @@
+//
+//  Questions.swift
+//  TrueFalseStarter
+//
+//  Created by Joel Strahan on 6/23/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+import GameKit
+import AudioToolbox
+
+class Questions {
+    var triviaQuestion: String
+    
+    init(question: String) {
+        self.triviaQuestion = question
+    }
+}
